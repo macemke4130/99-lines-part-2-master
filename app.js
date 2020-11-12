17a4699f08140f9bf44a720a7ed9eb09d211a424
Lucas Mace <lucasmace4130@gmail.com>
2020-11-11 21:48:53 -0600

    Need rest

--- app.js
+++ app.js
@@ -12,9 +12,20 @@ let penultimateLine = " line of code in the file";
 let lastLine1 = " line of code in the file, 1 line of code; ";
 let lastLine2 = " strikes one out, clears it all out, no more lines of code in the file";
 
-// Loop through the friends array --
+// Brains --
+let btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+    
+});
+
+let p = 1;
+
+let song = document.createElement("div");
+let songClass = song.className = "friend";
+console.log(song);
+
 for (let index = 0; index < friends.length; index++) {
-    console.log(friends[index] + ":"); // Displays Name: --
+    //console.log(friends[index] + ":"); // Displays Name: --
     let myCurrentFriend = index; // Stores Name for the next loop --
     // Loops through lyrics --
     for (let index = 99; index > 0; index--) {
@@ -22,16 +33,16 @@ for (let index = 0; index < friends.length; index++) {
             let nextLine = index - 1 // Creates varable for the final or next number in the lyric --
             // Compiles lyrics --
             let lyrics = index + line1 + index + line2 + friends[myCurrentFriend] + line3 + nextLine + line4;
-            console.log(lyrics);
+            //console.log(lyrics);
         } else if (index == 2) { // Catches if lyric should be "line" or "lines" --
             // Compiles lyrics --
             let nextLine = index - 1
             let lyrics = index + line1 + index + line2 + friends[myCurrentFriend] + line3 + nextLine + penultimateLine;
-            console.log(lyrics);
+            //console.log(lyrics);
         } else { // Catches the final lyric --
             // Compiles lyrics --
             let lyrics = index + lastLine1 + friends[myCurrentFriend] + lastLine2;
-            console.log(lyrics);
+            //console.log(lyrics);
         }
 
     }
